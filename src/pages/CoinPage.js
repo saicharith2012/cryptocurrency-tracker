b8d7104b29764e43a5e68788393643f8351ced2c
Akshith <akshithgandla7891@gmail.com>
2022-04-16 13:56:29 +0530

    made basic changes to CoinPage. js

---
+++ src/pages/CoinPage.js
@@ -0,0 +1,27 @@
+import { LinearProgress, makeStyles, Typography } from "@material-ui/core";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { SingleCoin } from "../config/api";
+import { CryptoState } from "../CryptoContext";
+
+const CoinPage = () => {
+  const { id } = useParams();
+  const [coin, setCoin] = useState();
+
+  const { currency, symbol } = CryptoState();
+
+  const fetchCoin = async () => {
+    const { data } = await axios.get(SingleCoin(id));
+
+    setCoin(data);
+  };
+
+  useEffect(() => {
+    fetchCoin();
+  }, []);
+
+  return <div>Coin Page</div>;
+};
+
+export default CoinPage;
