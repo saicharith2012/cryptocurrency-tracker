55cce086c7842e14eb7480204106662383eaa0f0
arlestt <jangatiloknath2002@gmail.com>
2022-04-19 14:59:48 +0530

    added carousel.js

---
+++ src/components/Banner/Carousel.js
@@ -0,0 +1,40 @@
+import { makeStyles } from "@material-ui/core";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import AliceCarousel from "react-alice-carousel";
+import { Link } from "react-router-dom";
+import { TrendingCoins } from "../../config/api";
+import { CryptoState } from "../../CryptoContext";
+import { numberWithCommas } from "../CoinsTable";
+
+const Carousel = () => {
+  const [trending, setTrending] = useState([]);
+  const { currency, symbol } = CryptoState();
+
+  const fetchTrendingCoins = async () => {
+    const { data } = await axios.get(TrendingCoins(currency));
+
+    console.log(data);
+    setTrending(data);
+  };
+
+  useEffect(() => {
+    fetchTrendingCoins();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currency]);
+
+  const useStyles = makeStyles((theme) => ({
+    carousel: {
+      height: "50%",
+      display: "flex",
+      alignItems: "center",
+    },
+    carouselItem: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      cursor: "pointer",
+      textTransform: "uppercase",
+      color: "white",
+    },
+  }));
