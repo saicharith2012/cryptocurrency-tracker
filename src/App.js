386648e8d30b321781730f5b10ae018ae23e8f53
arlestt <jangatiloknath2002@gmail.com>
2022-04-19 17:53:15 +0530

    Edited App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
 import './App.css';
+import Header from "./components/Header";
+
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
 
 function App() {
   return (
