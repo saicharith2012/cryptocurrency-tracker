3d15ccaa03c3fe5ac3d6cd55088cf40b45a87c1a
arlestt <jangatiloknath2002@gmail.com>
2022-04-20 21:51:41 +0530

    added a Banner image and index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import "react-alice-carousel/lib/alice-carousel.css";
+import CryptoContext from "./CryptoContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CryptoContext>
+      <App />
+    </CryptoContext>
   </React.StrictMode>,
   document.getElementById('root')
 );
