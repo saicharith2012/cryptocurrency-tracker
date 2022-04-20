e03f210d3db786211f3c2ab01a2c9e21e1b8c00f
saicharith2012 <saicharithpalabindhela@gmail.com>
2022-04-20 22:53:46 +0530

    edited Banner.js

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -41,7 +41,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Crypto Hunter
+            Crypto Tracker
           </Typography>
           <Typography
             variant="subtitle2"
@@ -51,7 +51,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Get all the Info regarding your favorite Crypto Currency
+            Know all the Info about your favorite Crypto Currency
           </Typography>
         </div>
         <Carousel />
