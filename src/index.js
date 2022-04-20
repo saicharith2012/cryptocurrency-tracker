6e0acce7bb9ca98d05baab4dbb00426696e62de4
saicharith2012 <saicharithpalabindhela@gmail.com>
2022-04-20 23:36:21 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import "react-alice-carousel/lib/alice-carousel.css";
 import CryptoContext from "./CryptoContext";
 
@@ -11,5 +11,5 @@ ReactDOM.render(
       <App />
     </CryptoContext>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
