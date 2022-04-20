14dd69a6335985a7b1cf15c81e391b5e69dff2ca
saicharith2012 <saicharithpalabindhela@gmail.com>
2022-04-20 22:50:41 +0530

    edited Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -49,7 +49,7 @@ import {
                 variant="h6"
                 className={classes.title}
               >
-                Crypto Hunter
+                Crypto Tracker
               </Typography>
               {/* <Button color="inherit">Login</Button> */}
               <Select
